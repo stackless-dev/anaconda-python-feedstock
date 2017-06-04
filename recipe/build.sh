3f9c0b8e8e0daf1ec3fd1fef9d4a8be83e55919e
Ray Donnelly <mingw.android@gmail.com>
2017-06-04 18:15:11 +0100

    Add cross-compilation support and (enabled for now) _OPTIMIZED flag (WIP)
    Need to ensure lto is being used and is working properly here.
    
    Also changes are needed to sysconfig so it falls back to sensible
    defaults if the pseudo-cross compilers are not found.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,15 @@
 # tries to update bzip2.
 find "${PREFIX}/lib" -name "libbz2*${SHLIB_EXT}*" | xargs rm -fv {}
 
+# Prevent lib/python3.6/_sysconfigdata_m_linux_arm-linux-gnueabi.py from ending up with full paths to these
+# things in _build_env because _build_env will not get found during prefix replacement, only _h_env_placeh ...
+AR=$(basename "${AR}")
+# CC must contain the string 'gcc' or else distutils thinks it is on macOS and uses '-R' to set rpaths.
+CC=$(basename "${GCC}")
+CXX=$(basename "${CXX}")
+RANLIB=$(basename "${RANLIB}")
+READELF=$(basename "${READELF}")
+
 ${SYS_PYTHON} ${RECIPE_DIR}/brand_python.py
 
 # Remove test data to save space.
@@ -27,15 +36,64 @@ elif [ $(uname) == Linux ]; then
   export LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
 fi
 
-./configure --enable-shared \
+if [[ "${BUILD}" != "${HOST}" ]]; then
+  # Build the exact same Python for the build machine. It would be nice (and might be
+  # possible already?) to be able to make this just an 'exact' pinned build dependency
+  # of a split-package?
+  BUILD_PYTHON_PREFIX=${PWD}/build-python-install
+  mkdir build-python-build
+  pushd build-python-build
+    (unset CPPFLAGS LDFLAGS;
+     export CC=/usr/bin/gcc \
+            CXX=/usr/bin/g++ \
+            CPP=/usr/bin/cpp \
+            CFLAGS="-O2" \
+            AR=/usr/bin/ar \
+            RANLIB=/usr/bin/ranlib \
+            LD=/usr/bin/ld && \
+      ../configure --build=${BUILD} \
+                   --host=${BUILD} \
+                   --prefix=${BUILD_PYTHON_PREFIX} \
+                   --with-ensurepip=no && \
+      make && \
+      make install)
+    export PATH=${BUILD_PYTHON_PREFIX}/bin:${PATH}
+    ln -s ${BUILD_PYTHON_PREFIX}/bin/python3.6 ${BUILD_PYTHON_PREFIX}/bin/python
+  popd
+  echo "ac_cv_file__dev_ptmx=yes"        > config.site
+  echo "ac_cv_file__dev_ptc=yes"        >> config.site
+  echo "ac_cv_pthread=yes"              >> config.site
+  echo "ac_cv_little_endian_double=yes" >> config.site
+  export CONFIG_SITE=${PWD}/config.site
+  # This is needed for libffi:
+  export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+  _OPTIMISED=1
+else
+  _OPTIMISED=1
+fi
+
+declare -a _extra_opts
+if [[ ${_OPTIMIZED} == 1 ]]; then
+  _extra_opts+=(--enable-optimizations)
+  _extra_opts+=(--enable-lto)
+  _MAKE_TARGET=profile-opt
+else
+  _MAKE_TARGET=
+fi
+
+./configure --build=${BUILD} \
+            --host=${HOST} \
+            --enable-shared \
             --enable-ipv6 \
             --with-ensurepip=no \
             --prefix=$PREFIX \
+            --with-system-ffi \
             --with-tcltk-includes="-I$PREFIX/include" \
-            --with-tcltk-libs="-L$PREFIX/lib -ltcl8.5 -ltk8.5" \
-            --enable-loadable-sqlite-extensions
+            --with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
+            --enable-loadable-sqlite-extensions \
+            "${_extra_opts[@]}"
 
-make
+make ${_MAKE_TARGET}
 make install
 ln -s $PREFIX/bin/python3.6 $PREFIX/bin/python
 ln -s $PREFIX/bin/pydoc3.6 $PREFIX/bin/pydoc
