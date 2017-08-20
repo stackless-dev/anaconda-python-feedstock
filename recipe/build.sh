88d73e616af9e1f6175141b08e332fdc3f5bb841
Ray Donnelly <mingw.android@gmail.com>
2017-08-20 13:48:23 +0100

    Build fixes for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,15 +12,32 @@ find "${PREFIX}/lib" -name "libbz2*${SHLIB_EXT}*" | xargs rm -fv {}
 # Prevent lib/python${VER}/_sysconfigdata_*.py from ending up with full paths to these things
 # in _build_env because _build_env will not get found during prefix replacement, only _h_env_placeh ...
 AR=$(basename "${AR}")
+
 # CC must contain the string 'gcc' or else distutils thinks it is on macOS and uses '-R' to set rpaths.
-CC=$(basename "${GCC}")
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CC=$(basename "${CC}")
+else
+  CC=$(basename "${GCC}")
+fi
 CXX=$(basename "${CXX}")
 RANLIB=$(basename "${RANLIB}")
 READELF=$(basename "${READELF}")
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  LDFLAGS=${LDFLAGS_CC}
+fi
+
+if [[ ${HOST} =~ .*darwin.* ]] && [[ -n ${CONDA_BUILD_SYSROOT} ]]; then
+  # Python's setup.py will figure out that this is a macOS sysroot.
+  CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${CFLAGS}
+  LDFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${LDFLAGS}
+  CPPFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${CPPFLAGS}
+fi
+
 # Debian uses -O3 then resets it at the end to -O2 in _sysconfigdata.py
 export CFLAGS=$(echo "${CFLAGS}" | sed "s/-O2/-O3/g")
 export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-O2/-O3/g")
+export LDFLAGS
 
 if [[ ${CONDA_FORGE} == yes ]]; then
   ${SYS_PYTHON} ${RECIPE_DIR}/brand_python.py
@@ -31,10 +48,10 @@ rm -rf Lib/ensurepip
 
 export CPPFLAGS=${CPPFLAGS}" -I${PREFIX}/include"
 export LDFLAGS=${LDFLAGS}" -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
-if [[ $(uname) == Darwin ]]; then
+if [[ ${HOST} =~ .*darwin.* ]]; then
   sed -i -e "s/@OSX_ARCH@/$ARCH/g" Lib/distutils/unixccompiler.py
   UNICODE=ucs2
-elif [[ $(uname) == Linux ]]; then
+elif [[ ${HOST} =~ .*linux.* ]]; then
   export LDFLAGS=${LDFLAGS}" -Wl,--no-as-needed"
   UNICODE=ucs4
 fi
@@ -62,4 +79,33 @@ pushd ${PREFIX}/lib/python${VER}
   mv test/__init__.py test/test_support* test/script_helper* test_keep/
   rm -rf test */test
   mv test_keep test
+popd
+
+# Size reductions:
+pushd ${PREFIX}
+  found_lib_libpython_a=no
+  if [[ -f lib/libpython${VER}.a ]]; then
+    found_lib_libpython_a=yes
+    chmod +w lib/libpython${VER}.a
+    if [[ -n ${HOST} ]]; then
+      ${HOST}-strip -S lib/libpython${VER}.a
+    else
+      strip -S lib/libpython${VER}.a
+    fi
+  fi
+  CONFIG_LIBPYTHON=$(find lib/python${VER}/config -name "libpython${VER}.a")
+  if [[ -f ${CONFIG_LIBPYTHON} ]]; then
+    if [[ ${found_lib_libpython_a} == yes ]]; then
+      chmod +w ${CONFIG_LIBPYTHON}
+      rm ${CONFIG_LIBPYTHON}
+      ln -s ../../libpython${VER}.a ${CONFIG_LIBPYTHON}
+    else
+      chmod +w ${CONFIG_LIBPYTHON}
+      if [[ -n ${HOST} ]]; then
+        ${HOST}-strip -S ${CONFIG_LIBPYTHON}
+      else
+        strip -S ${CONFIG_LIBPYTHON}
+      fi
+	fi
+  fi
 popd
