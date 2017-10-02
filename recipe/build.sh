b5c3f1770dfa2644893fdd154a9b571e5d4ff4ea
Michael Sarahan <msarahan@gmail.com>
2017-10-02 14:46:33 -0500

    replace - and . with _ in sysconfig output name

--- recipe/build.sh
+++ recipe/build.sh
@@ -151,7 +151,7 @@ popd
 #   to the name of this file (minus the .py extension)
 pushd $PREFIX/lib/python2.7
 recorded_name=$(find . -name "_sysconfig*.py")
-mv $recorded_name _sysconfig_${HOST//-/_}.py
+mv $recorded_name _sysconfig_$(echo ${HOST} | sed -e 's/[.-]/_/g').py
 if [[ ${HOST} =~ .*darwin.* ]]; then
     cp $RECIPE_DIR/default_sysconfig_osx.py $recorded_name
 else
