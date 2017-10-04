97e0dd868bae82c13ba211eba1b246d6c16583f1
Ray Donnelly <mingw.android@gmail.com>
2017-10-04 17:33:07 +0100

    Add -Wl,--sysroot=/ to the linux sysconfigdata fallback

--- recipe/build.sh
+++ recipe/build.sh
@@ -150,12 +150,12 @@ popd
 #   using the new compilers with python will require setting _PYTHON_SYSCONFIGDATA_NAME
 #   to the name of this file (minus the .py extension)
 pushd $PREFIX/lib/python2.7
-recorded_name=$(find . -name "_sysconfig*.py")
-mv $recorded_name _sysconfig_$(echo ${HOST} | sed -e 's/[.-]/_/g').py
+recorded_name=$(find . -name "_sysconfigdata*.py")
+mv $recorded_name _sysconfigdata_$(echo ${HOST} | sed -e 's/[.-]/_/g').py
 if [[ ${HOST} =~ .*darwin.* ]]; then
-    cp $RECIPE_DIR/default_sysconfig_osx.py $recorded_name
+    cp $RECIPE_DIR/default_sysconfigdata_osx.py $recorded_name
 else
-    cp $RECIPE_DIR/default_sysconfig_linux.py $recorded_name
+    cp $RECIPE_DIR/default_sysconfigdata_linux.py $recorded_name
     mkdir -p $PREFIX/compiler_compat
     cp $LD $PREFIX/compiler_compat/ld
     echo "Files in this folder are to enhance backwards compatibility of anaconda software with older compilers.  See https://github.com/conda/conda/issues/6030 for more information."  > $PREFIX/compiler_compat/README
