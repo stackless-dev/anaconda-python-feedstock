4f5155ca21466fee90db12fde5766d938d3ef07f
Ray Donnelly <mingw.android@gmail.com>
2017-11-03 09:05:20 +0000

    Set Py_ENABLE_SHARED to 0 / use the correct sysconfigdata

--- recipe/default_sysconfigdata_linux.py
+++ recipe/default_sysconfigdata_linux.py
@@ -608,7 +608,7 @@ build_time_vars = {'ABIFLAGS': 'm',
                '-Wl,-rpath=/opt/anaconda1anaconda2anaconda3/lib -Wl,--no-as-needed '
                '-Wl,--sysroot=/',
  'Py_DEBUG': 0,
- 'Py_ENABLE_SHARED': 1,
+ 'Py_ENABLE_SHARED': 0,
  'Py_HASH_ALGORITHM': 0,
  'QUICKTESTOPTS': '-x test_subprocess test_io test_lib2to3 \\',
  'RANLIB': 'ranlib',
